fbb1ba4a64d7601e870b5dd0232846835702eadb
Jerrypoisz <jerry.poi.sz@gmail.com>
2017-12-26 12:59:59 +0800

    JZHTTPD can now take in income request and handle GET method

--- main.c
+++ main.c
@@ -7,44 +7,138 @@
 //
 
 #include <stdio.h>
-#include <sys/socket.h>
+#include <sys/socket.h>//send , socket;
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <netinet/in.h>// sockaddr_in
+//#include <arpa/inet.h>
+#include <unistd.h> //write();
 #include <ctype.h>
 #include <strings.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <stdlib.h>
-#include <stdint.h>
-//int acceptRequest(int serverSock);
+//#include <stdint.h>
+#define SERVER_STRING "Server: Jerry's WEBSERVER Running JZHTTPD\r\n"
 int serverStartUp(u_int16_t *port);
 void errorDie(char * stirng);
-int acceptRequest(char * buffer,char * response);
+void unimplemented(int client);
+void notFound(int client);
+void header(int client,FILE * file);
+void sendFile(int client,FILE * file);
+void sendFile(int client,FILE * file) {
+    char usingString[1024];
+    fgets(usingString, sizeof(usingString), file);
+    while(!feof(file)) {
+        write(client, usingString, strlen(usingString));
+        fgets(usingString, sizeof(usingString), file);
+    }
+}
+void header(int client,FILE * file) {
+    char usingString[1024];
+    strcpy(usingString, "HTTP/1.1 200 OK\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    write(client, SERVER_STRING, strlen(SERVER_STRING));
+    
+    strcpy(usingString, "Conten-Tpye: text/html\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "\r\n");
+    write(client, usingString, strlen(usingString));
+}
+void notFound(int client) {
+    char usingString[1024];
+    strcpy(usingString, "HTTP/1.1 404 Not Found\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, SERVER_STRING);
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "Content-Type: text/html\r\n\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "<HTML><HEAD><TITLE>File Not Found</TITLE>\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "<BODY><H>404 NOT FOUND</H><P>Sorry, the file you requested cannot be found on this server.</P>\r\n");
+    
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "</BODY></HTML>\r\n");
+    write(client, usingString, strlen(usingString));
+}
+void unimplemented(int client) {
+    char usingString[1024];
+    strcpy(usingString, "HTTP/1.0 501 Method Not Implemented\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, SERVER_STRING);
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "Content-Type: text/html\r\n\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "<HTML><HEAD><TITLE>Method Not Implemented</TITLE>\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "<BODY><H>501 Method UnImplemented</H><P>HTTP request method not supported.</P>\r\n");
+    write(client, usingString, strlen(usingString));
+    
+    strcpy(usingString, "</BODY></HTML>\r\n");
+    write(client, usingString, strlen(usingString));
+    
+}
+//int acceptRequest(char * buffer,char * response);
+int acceptRequest(char *buffer,int client) {
+    char url[255];
+    char method[255];
+    
+    int i = 0, j = 0;
+    while(!isspace(buffer[i]) && i < sizeof(method) - 1) {
+        method[i] = buffer[i];
+        i++;
+    }
+    method[i] = '\0';
+    while(isspace(buffer[i]))
+        i++;
+    while(!isspace(buffer[i]) && j < sizeof(url) - 1) {
+        url[j++] = buffer[i++];
+    }
+    url[j] = '\0';
+    if(strcmp(method, "GET") != 0) {
+        unimplemented(client);
+        return 0;
+    }
+    
+    if(url[strlen(url) - 1] == '/')
+        strcat(url, "index.html");
+    FILE * file = fopen(url, "r");
+    if(file == NULL) {
+        notFound(client);
+        return 0;
+    }
+    header(client, file);
+    sendFile(client,file);
+    return 1;
+}
+
 int serverStartUp(u_short *port) {
     int createSocket = 0;
-    struct sockaddr_in name;
-    //socklen_t nameLen = sizeof(name);
-    
-    int on = 1;
-    createSocket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in localhost;
+    int buff = 1;
+    createSocket = socket(AF_INET,SOCK_STREAM, 0); //AF_INET : IPv4
     if(createSocket == -1)
         errorDie("Setting up socket");
-    
-    memset(&name, 0, sizeof(name));
-    name.sin_family = AF_INET;
-    name.sin_addr.s_addr = htonl(INADDR_ANY);
-    name.sin_port = htons(*port);
-    
-    if((setsockopt(createSocket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0)
-        //setsockopt() manipulate the options associated with a socket.
+    memset(&localhost, 0, sizeof(localhost));
+    localhost.sin_family = AF_INET;
+    localhost.sin_addr.s_addr = htonl(INADDR_ANY);//(u_int32_t)0x00000000
+    localhost.sin_port = htons(*port);
+    if((setsockopt(createSocket, SOL_SOCKET, SO_REUSEADDR, &buff, sizeof(buff))) < 0)
+        //setsockopt() manipulates the options associated with a socket.
         errorDie("Setting sock opt");
-    if(bind(createSocket, (struct sockaddr *)&name , sizeof(name)) < 0)
+    if(bind(createSocket, (struct sockaddr *)&localhost , sizeof(localhost)) < 0)
         errorDie("Binding");
-    if(listen(createSocket, 5) < 0)
+    if(listen(createSocket, 5) < 0)//5 : Maximum pending connection.
         errorDie("Setting up listen");
     printf("Server startup succeeded.\n");
     return createSocket;
@@ -54,15 +148,9 @@ void errorDie(char * stirng) {
     printf("%s failure!\n",stirng); //TODO: change to perror?
     exit(-1);
 }
-
-int acceptRequest(char * buffer,char * response) {
-    return 1;
-}
 int main(int argc, const char * argv[]) {
     char *buffer;
     int buffLen = 1024 * sizeof(char);
-    int responseLen = 1024 * sizeof(char);
-    char *response = (char *)malloc(responseLen);
     buffer = malloc(buffLen);
     u_short port = 15000;
     int serverSock = serverStartUp(&port);
@@ -77,8 +165,7 @@ int main(int argc, const char * argv[]) {
             printf("Client conneted\n");
         recv(clientSocket, buffer, buffLen, 0);
         printf("%s",buffer);
-        acceptRequest(buffer, response);
-        write(clientSocket, "Hello World from JZHTTPD!\n", sizeof("Hello World from JZHTTPD!\n"));
+        acceptRequest(buffer,clientSocket);
         close(clientSocket);
     }
     return 0;
